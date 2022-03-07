7f9dea25fe2a0889ca206973af0b228d0bc1851b
Robert Clark <you@example.com>
2022-03-07 14:09:14 -0500

    Tweak to code and outputs following Ti'ms 3/6/2022 suggestions.

--- simple-test.R
+++ simple-test.R
@@ -2,8 +2,8 @@ library(knitr)
 library(here)
 library(kableExtra)
 
-outfile = here("test.pdf")
+outfile = here("test.png")
 table = head(iris)
-latex =  kable(x = table, format = "latex")
+latex =  kable(x = table, format = "html")
 as_image(x = latex, file = outfile)
   

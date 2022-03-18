45d5f43ca84473ed77d067d0fd57ee12c090611c
Tim <timothy.farkas@gmail.com>
2022-03-18 03:24:50 +0000

    update simple test to use tidytex

--- simple-test.R
+++ simple-test.R
@@ -1,9 +1,12 @@
 library(knitr)
 library(here)
 library(kableExtra)
+library(tinytex)
 
 outfile = here("test.png")
 table = head(iris)
-latex =  kable(x = table, format = "html")
-as_image(x = latex, file = outfile)
+latex =  kable(x = table, format = "latex", booktabs = TRUE, linesep = "")
+tryCatch(save_kable(x = latex, file = outfile, keep_tex = TRUE))
+tinytex::xelatex('test.tex')
+
   

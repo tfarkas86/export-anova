a3466557256dd4b3790cda0b6071536ef98e98ee
Tim <timothy.farkas@gmail.com>
2022-03-18 03:53:58 +0000

    update error handling

--- simple-test.R
+++ simple-test.R
@@ -3,10 +3,11 @@ library(here)
 library(kableExtra)
 library(tinytex)
 
-outfile = here("test.png")
-table = head(iris)
-latex =  kable(x = table, format = "latex", booktabs = TRUE, linesep = "")
-tryCatch(save_kable(x = latex, file = outfile, keep_tex = TRUE))
+outfile <- here("test.png")
+table <- head(iris)
+latex <-  kable(x = table, format = "latex", booktabs = TRUE, linesep = "")
+tryCatch(save_kable(x = latex, file = outfile, keep_tex = TRUE), 
+         error = function(e) {print("No PDF Created With kableExtra::save_kable()")})
 tinytex::xelatex('test.tex')
 
   

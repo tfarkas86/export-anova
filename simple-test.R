76b54396099866e963ef8197d3e929093dc1b506
tfarkas <timothy.farkas@gmail.com>
2022-03-18 20:58:55 -0600

    something simpletest

--- simple-test.R
+++ simple-test.R
@@ -10,12 +10,5 @@ tryCatch(save_kable(x = latex, file = outfile, keep_tex = TRUE),
          error = function(e) {print("No PDF Created With kableExtra::save_kable()")})
 tinytex::xelatex('test.tex')
 
-<<<<<<< HEAD
-outfile = here("test_table.png")
-table = head(iris)
-latex =  kable(x = table, format = "html")
-save_kable(x = latex, file = outfile)
-=======
->>>>>>> a3466557256dd4b3790cda0b6071536ef98e98ee
-  
+
   

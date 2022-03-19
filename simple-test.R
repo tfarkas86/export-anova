2a1225e4f6bc3905863f7b4525e23d7e273ebe90
tfarkas <timothy.farkas@gmail.com>
2022-03-18 21:00:38 -0600

    add test.tex; update simple-test

--- simple-test.R
+++ simple-test.R
@@ -3,12 +3,12 @@ library(here)
 library(kableExtra)
 library(tinytex)
 
-outfile <- here("test.png")
+outfile <- here("test.pdf")
 table <- head(iris)
 latex <-  kable(x = table, format = "latex", booktabs = TRUE, linesep = "")
 tryCatch(save_kable(x = latex, file = outfile, keep_tex = TRUE), 
          error = function(e) {print("No PDF Created With kableExtra::save_kable()")})
-tinytex::xelatex('test.tex')
+
 
 
   

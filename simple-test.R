98ef530c91fb349fd76dd860bf5d2ffa3b9fdd45
tfarkas <timothy.farkas@gmail.com>
2022-03-18 21:03:45 -0600

    keep tex to false

--- simple-test.R
+++ simple-test.R
@@ -6,7 +6,7 @@ library(tinytex)
 outfile <- here("test.pdf")
 table <- head(iris)
 latex <-  kable(x = table, format = "latex", booktabs = TRUE, linesep = "")
-tryCatch(save_kable(x = latex, file = outfile, keep_tex = TRUE), 
+tryCatch(save_kable(x = latex, file = outfile, keep_tex = FALSE), 
          error = function(e) {print("No PDF Created With kableExtra::save_kable()")})
 
 
